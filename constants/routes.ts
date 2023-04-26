dd956b054ef32efb3ba27b3d39883e606d5026bf
Daniel Ruciński <sanerw4@gmail.com>
2023-04-26 21:33:56 +0200

    changed social links

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,8 +1,8 @@
 export const DISCORD = "https://discord.com";
-export const TELEGRAM = "https://discord.com";
-export const REDDIT = "https://discord.com";
-export const TWITTER = "https://discord.com";
-export const EMAIL = "/";
+export const TELEGRAM = "https://t.me/pepet_io";
+export const REDDIT = "https://www.reddit.com/";
+export const TWITTER = "https://twitter.com/pepet_io";
+export const EMAIL = "";
 
 export const UNISWAP = "/";
 export const DEXTOOLS = "/";
